6a0e850e8466ddff0b82c1e5a847cf50efd87524
kuuchan-code <kuuchanxn@gmail.com>
2022-07-02 23:02:18 +0900

    train.pyでprefixを_ppo_cnn_rotateに、読み込むファイルを最新のファイルに。 ppo_zipfiles->models, ppo_tensorboard->tensorboard

--- train.py
+++ train.py
@@ -2,15 +2,20 @@
 """
 訓練用
 """
+import glob
+import os
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import CheckpointCallback
 from env import AnimalTower
 
 env = AnimalTower()
-model = PPO.load(path="ppo_zipfiles/rotate_200_steps",
-                 env=env, tensorboard_log="./ppo_tensorboard/")
+# 最新のモデルを読み込むように
+model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
+model = PPO.load(path=model_path,
+                 env=env, tensorboard_log="tensorboard")
 # model = PPO(policy='CnnPolicy', env=env,
-#             verbose=1, tensorboard_log="./ppo_tensorboard/")
-checkpoint_callback = CheckpointCallback(save_freq=100, save_path='./ppo_zipfiles/',
-                                         name_prefix='rotate')
+#             verbose=1, tensorboard_log="tensorboard")
+print(f"Loaded {model_path}")
+checkpoint_callback = CheckpointCallback(save_freq=100, save_path='models',
+                                         name_prefix='_rotate_cnn')
 model.learn(total_timesteps=1500, callback=[checkpoint_callback])
