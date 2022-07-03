eaf7e74b5f85faffa7c04f4da243d53746c70b17
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-04 01:09:44 +0900

    モデルのロード

--- train.py
+++ train.py
@@ -8,14 +8,20 @@ from stable_baselines3 import A2C
 from stable_baselines3.common.callbacks import CheckpointCallback
 from env import AnimalTower
 
-env = AnimalTower()
 # 最新のモデルを読み込むように
-# model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
-# model = A2C.load(path=model_path,
-#                 env=env, tensorboard_log="tensorboard")
-model = A2C(policy='CnnPolicy', env=env,
-            verbose=1, tensorboard_log="tensorboard")
+model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
+print(model_path)
+
+env = AnimalTower()
+
+# ファイルから
+model = A2C.load(path=model_path,
+                 env=env, tensorboard_log="tensorboard")
+
+# 初回から
+# model = A2C(policy='CnnPolicy', env=env,
+#             verbose=1, tensorboard_log="tensorboard")
 # print(f"Loaded {model_path}")
-checkpoint_callback = CheckpointCallback(save_freq=100, save_path='models',
+checkpoint_callback = CheckpointCallback(save_freq=20, save_path="models",
                                          name_prefix='_a2c_cnn_rotate_3d_bin')
 model.learn(total_timesteps=1500, callback=[checkpoint_callback])
