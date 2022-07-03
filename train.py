74fe399197496a95f74bc5de3a5ae1389e0ce622
kuuchan-code <kuuchanxn@gmail.com>
2022-07-04 00:52:50 +0900

    name_prefixを変数に

--- train.py
+++ train.py
@@ -8,7 +8,8 @@ from stable_baselines3 import A2C
 from stable_baselines3.common.callbacks import CheckpointCallback
 from env import AnimalTower
 
-env = AnimalTower()
+name_prefix = "_a2c_cnn_rotate_3d_bin"
+env = AnimalTower(log_path=name_prefix+".csv")
 # 最新のモデルを読み込むように
 # model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
 # model = A2C.load(path=model_path,
@@ -17,5 +18,5 @@ model = A2C(policy='CnnPolicy', env=env,
             verbose=1, tensorboard_log="tensorboard")
 # print(f"Loaded {model_path}")
 checkpoint_callback = CheckpointCallback(save_freq=100, save_path='models',
-                                         name_prefix='_a2c_cnn_rotate_3d_bin')
+                                         name_prefix=name_prefix)
 model.learn(total_timesteps=1500, callback=[checkpoint_callback])
