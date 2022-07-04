7b4e6c4221fe3c03d12bf3378fce86a7adca83af
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-04 18:28:32 +0900

    続きから学習

--- train.py
+++ train.py
@@ -14,15 +14,16 @@ now_str = datetime.now().strftime("%Y%m%d%H%M%S")
 print(f"{name_prefix}_{now_str}.csv")
 # assert False
 env = AnimalTower(log_path=f"log/{name_prefix}_{now_str}.csv")
+
 # 最新のモデルを読み込むように
-# model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
-# model = A2C.load(path=model_path,
-#                  env=env, tensorboard_log="tensorboard")
-# print(f"Loaded {model_path}")
+model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
+model = A2C.load(path=model_path,
+                 env=env, tensorboard_log="tensorboard")
+print(f"Loaded {model_path}")
 
 # 新規作成
-model = A2C(policy='CnnPolicy', env=env,
-            verbose=1, tensorboard_log="tensorboard")
+# model = A2C(policy='CnnPolicy', env=env,
+#             verbose=1, tensorboard_log="tensorboard")
 
 checkpoint_callback = CheckpointCallback(save_freq=50, save_path="models",
                                          name_prefix=name_prefix)
