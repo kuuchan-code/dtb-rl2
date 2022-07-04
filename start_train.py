f8f54ee5d66dd0af6fba7e5d080143afd0bf609e
kuuchan-code <kuuchanxn@gmail.com>
2022-07-04 21:34:19 +0900

    start_train.pyを修正

--- start_train.py
+++ start_train.py
@@ -8,20 +8,10 @@ from stable_baselines3 import A2C
 from stable_baselines3.common.callbacks import CheckpointCallback
 from env import AnimalTower
 
-name_prefix = "_a2c_cnn_rotate12_bin"
+name_prefix = "_r12m1sb_rewarddecay"
 env = AnimalTower(log_path=name_prefix+".csv")
-<<<<<<< HEAD:train.py
-# 最新のモデルを読み込むように
-#model_path = max(glob.glob("models/*.zip"), key=os.path.getctime)
-#model = A2C.load(path=model_path,
-#                env=env, tensorboard_log="tensorboard")
-#print(f"Loaded {model_path}")
-model = A2C(policy='CnnPolicy', env=env,
-             verbose=1, tensorboard_log="tensorboard")
-=======
 model = A2C(policy='CnnPolicy', env=env,
             verbose=1, tensorboard_log="tensorboard")
->>>>>>> 2ac94929a6087d52aa48c95e73cac21fbf055ca4:start_train.py
 checkpoint_callback = CheckpointCallback(save_freq=100, save_path='models',
                                          name_prefix=name_prefix)
 model.learn(total_timesteps=10000, callback=[checkpoint_callback])
