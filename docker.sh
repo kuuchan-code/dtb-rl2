1c34fdb3f35a8a9d21d0fa44530f4adfa95766ce
kuuchan-code <kuuchanxn@gmail.com>
2022-07-05 20:47:31 +0900

    docker.shを修正、rllibの動作確認用pythonファイルを設置

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ sudo docker pull rayproject/ray:latest-gpu
 # run rayproject/ray:latest-gpu
 # sudo docker run -t -i --gpus all --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu
 
-# install tensorflow
+# install tensorflow(in the docker)
 # pip install tensorflow
 
 # commit the container as rayproject/ray:latest-gpu-tensorflow
@@ -15,3 +15,6 @@ sudo docker pull rayproject/ray:latest-gpu
 
 # run rayproject/ray:latest-gpu-tensorflow
 # sudo docker run -t -i --runtime=nvidia --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
+
+# install tensorflow(in the docker)
+# python3 dtb-rl2/test/rllib.py
