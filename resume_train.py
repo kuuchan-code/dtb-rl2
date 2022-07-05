0091e01d6bf9f610971ce17ae8a355a9c3c76dde
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-05 19:44:42 +0900

    エラー処理追加 new file:   log/_a2c_cnn_r4m11b_20220705185915.csv modified:   resume_train.py

--- resume_train.py
+++ resume_train.py
@@ -8,12 +8,13 @@ from stable_baselines3 import A2C
 from stable_baselines3.common.callbacks import CheckpointCallback
 from env import AnimalTower
 from datetime import datetime
+from selenium.common.exceptions import WebDriverException
 
 # 識別子
 name_prefix = "_a2c_cnn_r4m11b"
 # 時刻
 now_str = datetime.now().strftime("%Y%m%d%H%M%S")
-print(f"{name_prefix}_{now_str}.csv")
+# print(f"{name_prefix}_{now_str}.csv")
 # assert False
 env = AnimalTower(log_path=f"log/{name_prefix}_{now_str}.csv")
 
@@ -30,5 +31,7 @@ checkpoint_callback = CheckpointCallback(save_freq=50, save_path="models",
                                          name_prefix=name_prefix)
 try:
     model.learn(total_timesteps=10000, callback=[checkpoint_callback])
+except WebDriverException as e:
+    print("接続切れ?")
 except KeyboardInterrupt as e:
     print("キーボード割り込み")
