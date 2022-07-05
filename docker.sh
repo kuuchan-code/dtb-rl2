ead69ba35d817efbc8f85bec891c541f44d07272
kuuchan-code <kuuchanxn@gmail.com>
2022-07-05 20:44:30 +0900

    docker.shにgpu版の導入スクリプトを書いた

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# pull rayproject/ray:latest-gpu
+sudo docker pull rayproject/ray:latest-gpu
+
+# run rayproject/ray:latest-gpu
+# sudo docker run -t -i --gpus all --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu
+
+# install tensorflow
+# pip install tensorflow
+
+# commit the container as rayproject/ray:latest-gpu-tensorflow
+# sudo docker ps
+# sudo docker commit コンテナID rayproject/ray:latest-gpu-tensorflow
+
+# run rayproject/ray:latest-gpu-tensorflow
+# sudo docker run -t -i --runtime=nvidia --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
