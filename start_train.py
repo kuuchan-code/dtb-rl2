dcff6e0d6fa4472cb5908d85c13348e833aaa306
kuuchan-code <kuuchanxn@gmail.com>
2022-07-06 18:59:59 +0900

    Rainbow DQN動いた、ちゃんと学習してそう

--- start_train.py
+++ start_train.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 import gym, ray
-from ray.rllib.agents import a3c
+from ray.rllib.agents import dqn
 from env import AnimalTower
-
 
 from ray.tune.registry import register_env
 
@@ -11,13 +10,34 @@ def env_creator(env_config):
 
 register_env("my_env", env_creator)
 
-ray.init()
-config = a3c.DEFAULT_CONFIG.copy()
-config["num_gpus"] = 1
-config["num_workers"] = 1
-config["framework"] = "tf2"
-config["log_level"] = "INFO"
-trainer = a3c.A3CTrainer(env="my_env", config=config)
+
+trainer = dqn.DQNTrainer(env="my_env", config={
+    "num_workers": 1,
+    "num_atoms": 51,
+    "noisy": True,
+    "gamma": 0.99,
+    "lr": .0001,
+    "hiddens": [512],
+    "rollout_fragment_length": 4,
+    "train_batch_size": 32,
+    "exploration_config": {
+        "epsilon_timesteps": 2,
+        "final_epsilon": 0.0,
+    },
+    "target_network_update_freq": 500,
+    "replay_buffer_config":{
+        "type": "MultiAgentPrioritizedReplayBuffer",
+        "prioritized_replay_alpha": 0.5,
+        "learning_starts": 10,
+        "capacity": 50000
+    },
+    "n_step": 3,
+    "model": {
+        "grayscale": True,
+        "zero_mean": False,
+        "dim": 42
+    }
+})
 
 for i in range(10000):
     print(trainer.train())
