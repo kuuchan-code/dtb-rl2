2bc0824722f4f7f9da404de3850bef1819546dec
kuuchan-code <kuuchanxn@gmail.com>
2022-07-07 13:10:35 +0900

    docker run時にタイムゾーンをjstに設定（etc/localtimeにシンボリックリンクを貼っても何故かうまくいかないため） modified:   docker.sh

--- docker.sh
+++ docker.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-pull rayproject/ray:latest-gpu
-# sudo docker pull rayproject/ray:latest-gpu
+# pull rayproject/ray:latest-gpu
+# docker pull rayproject/ray:latest-gpu
 
 # run rayproject/ray:latest-gpu
-# sudo docker run -t -i --gpus all --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu
+# docker run -it --gpus all -e TZ=Asia/Tokyo --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu
 
 # install tensorflow(in the docker)
 # pip install tensorflow
 
 # commit the container as rayproject/ray:latest-gpu-tensorflow
-# sudo docker ps
-# sudo docker commit コンテナID rayproject/ray:latest-gpu-tensorflow
+# docker ps
+# docker commit コンテナID rayproject/ray:latest-gpu-tensorflow
 
 # run rayproject/ray:latest-gpu-tensorflow
-# sudo docker run -t -i --runtime=nvidia --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
+# docker run -t -i --runtime=nvidia --shm-size=2.41gb -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
 
 # tensorflow with gpuとrllilbの動作確認(in the docker)
 # python3 dtb-rl2/test/is_gpu_available.py
@@ -24,4 +24,4 @@ pull rayproject/ray:latest-gpu
 # sudo apt update
 # sudo apt install libgl1-mesa-dev libopencv-dev
 
-# sudo docker run --network host -it --runtime=nvidia --shm-size=2.41gb --add-host=localhost_main:127.0.0.1 -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
+docker run --network host -it -e TZ=Asia/Tokyo --runtime=nvidia --shm-size=2.41gb --add-host=localhost_main:127.0.0.1 -v ~/dtb-rl2:/home/ray/dtb-rl2 rayproject/ray:latest-gpu-tensorflow
