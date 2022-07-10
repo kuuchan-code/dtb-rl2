98f6e180bfa6b6e1500f46744a9fa3762c5801e1
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-10 20:04:23 +0900

    エピソード数で停止するように変更

--- all_zero.py
+++ all_zero.py
@@ -8,11 +8,9 @@ from env import AnimalTower
 # 中央の6
 env = AnimalTower(udid="790908812299", log_prefix="all_6_560", x8_enabled=True)
 
-obs = env.reset()
 # 1000エピソードサンプルを集めたい
-for i in range(1000):
-    # action = 0
-    obs, reward, done, info = env.step(27)
-    env.render()
-    if done:
-        obs = env.reset()
+for i in range(1000-184):
+    obs = env.reset()
+    done = False
+    while not done:
+        obs, reward, done, info = env.step(27)
