d99e9dc6b9a1ba28f473e0b2f58d7321eee22213
kuuchan-code <kuuchanxn@gmail.com>
2022-07-12 13:59:49 +0900

    modified:   start_train.py

--- start_train.py
+++ start_train.py
@@ -4,23 +4,18 @@ import ray.tune
 from env import AnimalTower
 
 ray.tune.run(
-    'R2D2',
-    stop={
-        'training_iteration': 10000,
-    },
+    "R2D2",
     config={
-        'env':AnimalTower,
-        'framework': 'tf',
+        "env":AnimalTower,
+        "framework": "tf",
         # R2D2 settings.
-        'num_workers': 1,
-        'compress_observations': True,
-        'exploration_config': {'epsilon_timesteps': 40},
-        'target_network_update_freq': 10,
-        'model': {'use_lstm': True},
-        'timesteps_per_iteration': 1,
+        "num_workers": 1,
+        "exploration_config": {"epsilon_timesteps": 40},
+        "target_network_update_freq": 10,
+        "model": {"use_lstm": True, "max_seq_len": 4},
         "disable_env_checking": True
     },
     checkpoint_freq=100,
     checkpoint_at_end=True,
-    local_dir='checkpoints',
+    local_dir="checkpoints",
 )
