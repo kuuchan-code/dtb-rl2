a4b16d528dc749aad6dd915791074bafb6649d85
keiichiro-sonoda <21w2039g@shinshu-u.ac.jp>
2022-07-25 13:52:14 +0900

    乱数を0.5秒ごとに追加して描画した

--- draw_graph_imm.py
+++ draw_graph_imm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+import random
 from PyQt5.QtWidgets import QApplication, QWidget
-from pyqtgraph.Qt import QtGui, QtCore
+from PyQt5.QtCore import QTimer
 import numpy as np
 import pyqtgraph as pg
 import sys
@@ -13,10 +14,31 @@ class MyWidget(QWidget):
         pg.setConfigOptions(antialias=True)
         pg.setConfigOptions(foreground='k')
         pg.setConfigOptions(background='w')
+
         win = pg.GraphicsLayoutWidget(size=(400,300),border=True,parent=self)
         win.move(10,50)
 
+        graph = win.addPlot(title="Data")
+        graph.setLabel('left',"Power", units='W')
+        graph.setLabel('bottom',"Time", units='s')
 
+        self.counter = 0
+        self.x = []
+        self.y = []
+        self.curve = graph.plot(self.x,self.y,pen=pg.mkPen((120,23,200),width=2))
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.add_data)
+        self.timer.setSingleShot(True)
+        self.add_data()
+    
+    def add_data(self):
+        self.x.append(self.counter)
+        self.y.append(random.random())
+        self.counter += 1
+        self.curve.setData(self.x, self.y)
+        self.timer.start(500)
+
+    
 maxX = 100
 app = QApplication(sys.argv)
 gui = MyWidget()
